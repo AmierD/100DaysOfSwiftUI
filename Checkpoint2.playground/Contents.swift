cf26024abe2010086e2962be4765c6730a683557
amierld <amierLdavis@gmail.com>
2025-11-30 02:27:20 -0500

    Day 4 (Type Annotations vs Type Inference) & Checkpoint 2

---
+++ Checkpoint2.playground/Contents.swift
@@ -0,0 +1,7 @@
+import Cocoa
+
+let array = ["Google", "Apple", "Microsoft", "Google"]
+let set = Set(array)
+
+print("Number of items in the array: \(array.count)")
+print("Number of unique items in the array: \(set.count)")
---
+++ Day4.playground/Contents.swift
@@ -0,0 +1,16 @@
+import Cocoa
+
+// Day 4: Complex data types, part 2
+
+/* Type annotations */
+
+/*
+ - type inference:
+    - var score = 0
+    - var array = [5, 6, 8]
+    - var clues = [String]()
+ - type annotations
+    - var score: Int = 0
+    - var array: [Int] = [5, 6, 8]
+    - var cities: [String] = []
+ */
