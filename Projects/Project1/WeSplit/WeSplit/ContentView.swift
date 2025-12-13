7c4026caed5837740aeaa02f5e9f04208d651753
amierld <amierLdavis@gmail.com>
2025-12-13 16:01:36 -0500

    Day 17: Project 1 part 2 - applying knowledge of From, @State, Picker, and more.

--- Projects/Project1/WeSplit/WeSplit/ContentView.swift
+++ Projects/Project1/WeSplit/WeSplit/ContentView.swift
@@ -8,12 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var tapCount = 0
-    @State private var name = ""
-    let students = ["Amier", "John", "Sally"]
-    @State var currentStudent = "Amier"
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    @FocusState private var amountIsFocused: Bool
+    
+    private var totalPerPerson: Double {
+        let totalPer: Double = checkAmount / Double(numberOfPeople)
+        let tipAmount: Double = 1.0 + Double(tipPercentage) / 100.0
+        return totalPer * tipAmount
+    }
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
     var body: some View {
-        Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section("Payment Info") {
+                    TextField(
+                        "Amount",
+                        value: $checkAmount,
+                        format: .currency(
+                            // locale has all the user's region settings
+                            code: Locale.current.currency?.identifier ?? "USD"
+                        )
+                    )
+                    .keyboardType(.decimalPad)
+                    .focused($amountIsFocused)
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(0..<10) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                Section("Tip Percentage") {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                Section("Total Per Person") {
+                    Text(
+                        totalPerPerson,
+                        format: .currency(
+                            code: Locale
+                                .current
+                                .currency?
+                                .identifier ?? "USD"
+                        )
+                    )
+                }
+            }
+            
+            .navigationTitle(Text("WeSplit"))
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
+        }
     }
 }
 
