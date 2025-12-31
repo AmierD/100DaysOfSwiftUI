1de5bc12db0f167b948b7013d777e7b0238afeed
amierld <amierLdavis@gmail.com>
2025-12-31 12:13:46 -0500

    Day 21: Project 2 part 2

--- Projects/Project2/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ Projects/Project2/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,29 +8,85 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingAlert = false
+    @State private var countries = [
+        "Estonia",
+        "France",
+        "Germany",
+        "Ireland",
+        "Italy",
+        "Nigeria",
+        "Poland",
+        "Spain",
+        "UK",
+        "Ukraine",
+        "US"
+    ]
+        .shuffled()
+    @State private var correctAnswer = Int.random(in: 0..<3)
+    @State private var showingScore = false
+    @State private var scoreTitle = " "
+    @State private var isAnswerCorrect = false
+    
     var body: some View {
         ZStack {
-            VStack {
-                Text("Hello, World!")
-                Button("Edit", systemImage: "pencil", action: executeDelete)
-                    .buttonStyle(.borderedProminent)
-                    .tint(.mint)
-                Button("Show Alert") {
-                    showingAlert.toggle()
+            LinearGradient(
+                colors: [.blue, .indigo],
+                startPoint: .bottom,
+                endPoint: .top
+            )
+                .ignoresSafeArea()
+            
+            VStack(spacing: 30) {
+                Spacer()
+                VStack() {
+                    Text("Tap the flag of")
+                        .foregroundStyle(.white)
+                        .font(.subheadline.weight(.semibold))
+                    Text(countries[correctAnswer])
+                        .foregroundStyle(.white)
+                        .font(.largeTitle.weight(.heavy))
                 }
-                .alert("Important message", isPresented: $showingAlert) {
-                    Button("Delete", role: .destructive) { }
-                } message: {
-                    Text("Please read me")
+                Spacer()
+                ForEach(0..<3) { number in
+                    Button {
+                        flagTapped(number)
+                    } label: {
+                        Image(countries[number])
+                            .shadow(radius: 5)
+                    }
                 }
+                Spacer()
+                Spacer()
+                Text("Score: ???")
+                Spacer()
             }
+            .padding()
         }
-        .ignoresSafeArea()
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Play again", action: askQuestion)
+        } message: {
+            Text("Your score is ???")
+        }
     }
     
-    func executeDelete() {
-        print("Delete")
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+            isAnswerCorrect = true
+        } else {
+            scoreTitle = "Incorrect"
+            isAnswerCorrect = false
+        }
+        
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        
+        scoreTitle = " "
+        isAnswerCorrect = false
     }
 }
 
