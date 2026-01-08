15095e9b394e0b067c044e499655381a7e9ba273
amierld <amierLdavis@gmail.com>
2026-01-07 23:51:40 -0500

    Day 32: Project 6, part 1 - Animations

---
+++ Projects/Project6/Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Amier Davis on 1/7/26.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Projects/Project6/Animations/Animations/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by Amier Davis on 1/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var animationAmount = 0.0
+    
+    var body: some View {
+        Button("Tap Me") {
+            withAnimation {
+                animationAmount += 360
+            }
+        }
+        .padding(50)
+        .background(.red)
+        .foregroundStyle(.white)
+        .clipShape(.circle)
+        .rotation3DEffect(.degrees(animationAmount), axis: (x: 1, y: 0, z: 0))
+    }
+}
+
+#Preview {
+    ContentView()
+}
