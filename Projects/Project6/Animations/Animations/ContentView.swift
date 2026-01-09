cc23221f3f51c5276c589d9ec5cf9ff40617606b
amierld <amierLdavis@gmail.com>
2026-01-08 21:41:36 -0500

    Day 33: Project 6, part 2

--- Projects/Project6/Animations/Animations/ContentView.swift
+++ Projects/Project6/Animations/Animations/ContentView.swift
@@ -8,22 +8,84 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmount = 0.0
+    @State private var isShowingRed = false
     
     var body: some View {
-        Button("Tap Me") {
-            withAnimation {
-                animationAmount += 360
+        VStack {
+            Button("Tap me") {
+                withAnimation {
+                    isShowingRed.toggle()
+                }
+            }
+            
+            
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.pivot)
             }
         }
-        .padding(50)
-        .background(.red)
-        .foregroundStyle(.white)
-        .clipShape(.circle)
-        .rotation3DEffect(.degrees(animationAmount), axis: (x: 1, y: 0, z: 0))
+    }
+}
+
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+    
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(.degrees(amount), anchor: anchor)
+            .clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(active: CornerRotateModifier(amount: -90, anchor: .topLeading), identity: CornerRotateModifier(amount: 0, anchor: .topLeading))
     }
 }
 
 #Preview {
     ContentView()
 }
+
+/* Transitions */
+//VStack {
+//    Button("Tap me") {
+//        withAnimation {
+//            isShowingRed.toggle()
+//        }
+//    }
+//    
+//    
+//    if isShowingRed {
+//        Rectangle()
+//            .fill(.red)
+//            .frame(width: 200, height: 200)
+//            .transition(.asymmetric(insertion: .slide, removal: .scale))
+//    }
+//}
+
+/* Gesure animations */
+// @State private var dragAmount = CGSize.zero
+//        LinearGradient(colors: [.yellow, .red], startPoint: .topLeading, endPoint: .bottomTrailing)
+//                    .frame(width: 300, height: 200)
+//                    .clipShape(.rect(cornerRadius: 10))
+//                    .offset(dragAmount)
+//                    .gesture(
+//                        DragGesture()
+//                            .onChanged { dragAmount = $0.translation }
+//                            .onEnded { _ in dragAmount = .zero}
+//                    )
+//                    .animation(.bouncy, value: dragAmount)
+
+/* Order of the implicit animation modifier matters: */
+//        Button("Tap Me") {
+//            enabled.toggle()
+//        }
+//        .frame(width: 200, height: 200)
+//        .background(enabled ? .red : .green)
+//        .animation(nil, value: enabled)
+//        .clipShape(.rect(cornerRadius: enabled ? 60 : 0))
+//        .animation(.spring(duration: 0.4, bounce: 0.6), value: enabled)
