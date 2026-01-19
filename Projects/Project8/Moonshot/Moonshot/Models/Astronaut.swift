7a0516e30f33cc1e46d45fa65d36103c821c92c3
amierld <amierLdavis@gmail.com>
2026-01-18 20:40:34 -0500

    Day 40: Project 8, part 2

---
+++ Projects/Project8/Moonshot/Moonshot/Models/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Amier Davis on 1/18/26.
+//
+
+import SwiftUI
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ Projects/Project8/Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Amier Davis on 1/16/26.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
