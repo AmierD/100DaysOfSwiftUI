970346656c0ce022955b361351c358024ee63f98
AmierD <amierLdavis@gmail.com>
2026-01-29 00:05:41 -0500

    Day 50: Project 10, part 2 - Making classes properly codable, advanced haptics with CoreHaptics

--- Projects/Project10/CupcakeCorner/CupcakeCorner/Views/ContentView.swift
+++ Projects/Project10/CupcakeCorner/CupcakeCorner/Views/ContentView.swift
@@ -6,76 +6,75 @@
 //
 
 import SwiftUI
+import CoreHaptics
 
 struct ContentView: View {
-    @State private var results = [Result]()
-    
-    @State private var username = ""
-    @State private var email = ""
-    var disableForm: Bool {
-        username.count < 5 || email.count < 5
-    }
+    @State private var counter = 0
+    @State private var engine: CHHapticEngine?
     var body: some View {
-        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { image in
-            image
-                .resizable()
-                .scaledToFit()
-        } placeholder: {
-            ProgressView()
-        }
-        .frame(width: 200, height: 200)
-        AsyncImage(url: URL(string: "https://hws.dev/img/bad.png")) { phase in
-            if let image = phase.image {
-                image
-                    .resizable()
-                    .scaledToFit()
-            } else if phase.error != nil {
-                Text("There was an error loading the image.")
-            } else {
-                ProgressView()
+        VStack {
+            Button("Encode Taylor", action: encodeTaylor)
+            Button("Tap count: \(counter)") {
+                counter += 1
             }
-        }
-        .frame(width: 200, height: 200)
-        Form {
-            Section {
-                TextField("Username", text: $username)
-                TextField("Email", text: $email)
-            }
-            
-            Section {
-                // views other than buttons can use the disabled modifier
-                Button("Create account") {
-                    print("Creating account")
-                }
-                .disabled(disableForm)
-            }
-        }
-        List(results, id: \.trackId) { item in
-            VStack(alignment: .leading) {
-                Text(item.trackName)
-                    .font(.headline)
-                Text(item.collectionName)
-            }
-        }
-        .task {
-            await loadData()
+            .sensoryFeedback(.increase, trigger: counter)
+            // .sensoryFeedback(.impact(weight: .heavy, intensity: 1), trigger: counter)
+            Button("Complex haptics", action: complexSuccess)
+                .onAppear(perform: prepareHaptics)
         }
     }
     
-    func loadData() async {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-            print("Invalid URL")
-            return
-        }
+    func encodeTaylor() {
+        let data = try! JSONEncoder().encode(User())
+        let str = String(decoding: data, as: UTF8.self)
+        print(str) // erroniously prints {"_name":"Taylor","_$observationRegistrar":{}} before adding CodingKeys enum to User
+    }
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+
         do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                results = decodedResponse.results
-            }
+            engine = try CHHapticEngine()
+            try engine?.start()
         } catch {
-            print("Invalid data")
+            print("There was an error creating the engine: \(error.localizedDescription)")
         }
     }
+    func complexSuccess() {
+        // make sure that the device supports haptics
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        var events = [CHHapticEvent]()
+
+//        for i in stride(from: 0, to: 1, by: 0.1) {
+//            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
+//            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
+//            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: i)
+//            events.append(event)
+//        }
+        
+        for i in stride(from: 0, to: 1, by: 0.1) {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(1 - i))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(1 - i))
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 1 + i)
+            events.append(event)
+        }
+
+        // convert those events into a pattern and play it immediately
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play pattern: \(error.localizedDescription).")
+        }
+    }
+}
+
+@Observable
+class User: Codable {
+    enum CodingKeys: String, CodingKey {
+        case _name = "name"
+    }
+    var name = "Taylor"
 }
 
 #Preview {
