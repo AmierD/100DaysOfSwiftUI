7bdb2669d8e1119cb83c669205b25ab14027a1fc
AmierD <amierLdavis@gmail.com>
2026-02-23 15:06:26 -0500

    Day 62: Project 13, part 1

---
+++ Projects/Project13/Instafilter/Instafilter/ContentView.swift
@@ -0,0 +1,49 @@
+//
+//  ContentView.swift
+//  Instafilter
+//
+//  Created by Amier Davis on 2/23/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var blurAmount = 0.0
+    @State private var showingConfirmation = false
+    @State private var backgroundColor: Color = .white
+    
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+                .blur(radius: blurAmount)
+            
+            Slider(value: $blurAmount, in: 0...20)
+                .onChange(of: blurAmount) { oldValue, newValue in
+                    print("New value is \(blurAmount)")
+                }
+            
+            Button("Random Blur") {
+                blurAmount = Double.random(in: 0...20)
+            }
+            
+            Button("Change Background") {
+                showingConfirmation = true
+            }
+            .confirmationDialog("Change Background", isPresented: $showingConfirmation) {
+                Button("Red") { backgroundColor = .red }
+                Button("Green") { backgroundColor = .green }
+                Button("Red") { backgroundColor = .blue }
+                Button("Cancel", role: .cancel) { }
+            } message: {
+                Text("Select a new color.")
+            }
+            .frame(width: 300, height: 300)
+            .background(backgroundColor)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Projects/Project13/Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Amier Davis on 2/23/26.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
