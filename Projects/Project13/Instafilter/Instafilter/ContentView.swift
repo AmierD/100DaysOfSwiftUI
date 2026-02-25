8c862c14ca7f170bbd76e727d148793c17647d9f
AmierD <amierLdavis@gmail.com>
2026-02-25 14:47:35 -0500

    Day 65: Project 13, part 4

--- Projects/Project13/Instafilter/Instafilter/ContentView.swift
+++ Projects/Project13/Instafilter/Instafilter/ContentView.swift
@@ -8,92 +8,80 @@
 import CoreImage
 import CoreImage.CIFilterBuiltins
 import PhotosUI
-import StoreKit
 import SwiftUI
 
 struct ContentView: View {
-    @State private var image: Image?
+    @State private var processedImage: Image?
+    @State private var filterIntensity = 0.5
     
-    @State private var pickerItems = [PhotosPickerItem]()
-    @State private var selectedImages = [Image]()
+    @State private var selectedItem: PhotosPickerItem?
     
-    @Environment(\.requestReview) var requestReview
-    
-    let example = Image(.example)
+    @State private var currentFilter = CIFilter.sepiaTone()
+    let context = CIContext()
     
     var body: some View {
-        VStack {
+        NavigationStack {
             VStack {
-                PhotosPicker("Select a picture", selection: $pickerItems, maxSelectionCount: 3, matching: .images)
-                ScrollView {
-                    ForEach(0..<selectedImages.count, id: \.self) { i in
-                        selectedImages[i]
+                Spacer()
+                PhotosPicker(selection: $selectedItem) {
+                    if let processedImage {
+                        processedImage
                             .resizable()
                             .scaledToFit()
+                    } else {
+                        ContentUnavailableView(
+                            "No Picture",
+                            systemImage: "photo.badge.plus",
+                            description: Text("Tap to import a photo")
+                        )
                     }
                 }
-            }
-            .onChange(of: pickerItems) {
-                Task {
-                    selectedImages.removeAll()
+                .onChange(of: selectedItem, loadImage)
+                
+                Spacer()
+                
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                        .onChange(of: filterIntensity, applyProcessing)
+                }
+                .padding(.vertical)
+                
+                HStack {
+                    Button("Change Filter", action: changeFilter)
                     
-                    for  item in pickerItems {
-                        if let loadedImage = try await item.loadTransferable(type: Image.self) {
-                            selectedImages.append(loadedImage)
-                        }
-                    }
+                    Spacer()
+                    
                 }
             }
-            ShareLink(item: URL(string: "https://www.hackingwithswift.com")!) {
-                Label("Learn more about Swift", systemImage: "swift")
-            }
-            ShareLink(item: example, preview: SharePreview("Cartman", image: example)) {
-                Label("Share Cartman", systemImage: "square.and.arrow.up")
-            }
-            image?
-                .resizable()
-                .scaledToFit()
-            ContentUnavailableView(
-                "No snippets",
-                systemImage: "swift",
-                description: Text("You don't have any saved snippets yet.")
-            )
-        }
-        .onAppear(perform: loadImage)
-        .onAppear {
-            requestReview()
+            .padding([.horizontal, .bottom])
+            .navigationTitle("Instafilter")
         }
     }
     
+    func changeFilter() {
+        
+    }
+    
     func loadImage() {
-        let inputImage = UIImage(resource: .example)
-        let beginImage = CIImage(image: inputImage)
-        
-        let context = CIContext()
-        
-        let currentFilter = CIFilter.twirlDistortion()
-        currentFilter.inputImage = beginImage
-        
-        let amount = 0.4
-        
-        let inputKeys = currentFilter.inputKeys
-        
-        if inputKeys.contains(kCIInputIntensityKey) {
-            currentFilter.setValue(amount, forKey: kCIInputIntensityKey)
+        Task {
+            guard let imageData = try await selectedItem?.loadTransferable(type: Data.self) else { return }
+            guard let inputImage = UIImage(data: imageData) else { return }
+            
+            let beginImage = CIImage(image: inputImage)
+            currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+            applyProcessing()
         }
-        if inputKeys.contains(kCIInputRadiusKey) {
-            currentFilter.setValue(amount * 200, forKey: kCIInputRadiusKey)
-        }
-        if inputKeys.contains(kCIInputScaleKey) {
-            currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey)
-        }
+    }
+    
+    func applyProcessing() {
+        currentFilter.intensity = Float(filterIntensity)
         
         guard let outputImage = currentFilter.outputImage else { return }
-        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else {
-            return
-        }
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
+        
         let uiImage = UIImage(cgImage: cgImage)
-        image = Image(uiImage: uiImage)
+        processedImage = Image(uiImage: uiImage)
     }
 }
 
