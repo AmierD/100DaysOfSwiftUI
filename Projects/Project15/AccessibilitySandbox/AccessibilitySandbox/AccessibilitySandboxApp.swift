e38d6aa35536ee5281dd08d8a80d2a16c584216b
AmierD <amierLdavis@gmail.com>
2026-03-24 12:25:07 -0400

    Day 74: Project 15, part 1

---
+++ Projects/Project15/AccessibilitySandbox/AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Amier Davis on 3/24/26.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
