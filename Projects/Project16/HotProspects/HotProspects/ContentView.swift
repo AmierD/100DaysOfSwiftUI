3c89fa5228c015f7f39488a2ec6c43ac0e9f1ba0
AmierD <amierLdavis@gmail.com>
2026-06-30 23:49:44 -0400

    Day 81: Project 16, part 3

--- Projects/Project16/HotProspects/HotProspects/ContentView.swift
+++ Projects/Project16/HotProspects/HotProspects/ContentView.swift
@@ -5,21 +5,31 @@
 //  Created by Amier Davis on 6/26/26.
 //
 
+import SamplePackage
 import SwiftUI
+import UserNotifications
 
 struct ContentView: View {
     let users = ["Tohru", "Yuki", "Kyo", "Momiji"]
     @State private var selection = Set<String>()
-    
+
     @State private var selectedTab = "Three"
-    
+
     @State private var output = ""
     
+    var results: String {
+        let possibleNumbers = 1...60
+        let selected = possibleNumbers.random(7).sorted()
+        let strings = selected.map(String.init)
+        
+        return strings.formatted()
+    }
+
     enum BackgroundColors: String {
         case red = "Red"
         case orange = "Orange"
         case green = "Green"
-        
+
         var color: Color {
             switch self {
             case .red: return .red
@@ -28,34 +38,37 @@ struct ContentView: View {
             }
         }
     }
-    
+
     @State private var backgroundColor: BackgroundColors = .red
-    
-    
+
     var body: some View {
         TabView(selection: $selectedTab) {
             Tab("One", systemImage: "star", value: "One") {
                 selectionView
             }
-            
+
             Tab("Two", systemImage: "circle", value: "Two") {
                 resultView
                 Button("Show tab 1") {
                     selectedTab = "One"
                 }
+                Text(results)
             }
-            
+
             Tab("Three", systemImage: "square", value: "Three") {
                 imgInterpolationView
             }
-            
+
             Tab("Context", systemImage: "square", value: "Context") {
                 contextMenuView
             }
-
+            
+            Tab("Notis", systemImage: "bell", value: "Notification") {
+                notificationsView
+            }
         }
     }
-    
+
     var selectionView: some View {
         NavigationStack {
             VStack {
@@ -67,36 +80,50 @@ struct ContentView: View {
                 }
             }
             .toolbar {
-                    EditButton()
+                EditButton()
             }
-            
+            List {
+                Text("Bob")
+                    .swipeActions {
+                        Button("Send message", systemImage: "message") {
+                            print("Hi")
+                        }
+                    }
+                    .swipeActions(edge: .leading) {
+                        Button("Pin", systemImage: "pin") {
+                            print("Pinning")
+                        }
+                        .tint(.orange)
+                    }
+            }
+
             Button("Show tab 2") {
                 selectedTab = "Two"
             }
         }
     }
-    
+
     var resultView: some View {
         Text(output)
             .task {
                 await fetchReadings()
             }
     }
-    
+
     var imgInterpolationView: some View {
         Image(.example)
-            .interpolation(.none) // allows image to retain pixellated look
+            .interpolation(.none)  // allows image to retain pixellated look
             .resizable()
             .scaledToFit()
             .background(.black)
     }
-    
+
     var contextMenuView: some View {
         VStack {
             Text("Hello World")
                 .padding()
                 .background(backgroundColor.color)
-            
+
             Text("Change Color")
                 .padding()
                 .contextMenu {
@@ -112,7 +139,42 @@ struct ContentView: View {
                 }
         }
     }
-    
+
+    var notificationsView: some View {
+        VStack {
+            Button("Request Permission") {
+                UNUserNotificationCenter.current().requestAuthorization(
+                    options: [.alert, .badge, .sound]) { success, error in
+                        if success {
+                            print("All set!")
+                        } else if let error {
+                            print(error.localizedDescription)
+                        }
+                    }
+            }
+
+            Button("Schedule Notificaiton") {
+                let content = UNMutableNotificationContent()
+                content.title = "Feed the cat"
+                content.subtitle = "It looks hungry"
+                content.sound = UNNotificationSound.default
+
+                let trigger = UNTimeIntervalNotificationTrigger(
+                    timeInterval: 5,
+                    repeats: false
+                )
+
+                let request = UNNotificationRequest(
+                    identifier: UUID().uuidString,
+                    content: content,
+                    trigger: trigger
+                )
+                
+                UNUserNotificationCenter.current().add(request)
+            }
+        }
+    }
+
     func getSysImg(_ str: String) -> String {
         if str == backgroundColor.rawValue {
             "checkmark.circle.fill"
@@ -120,25 +182,25 @@ struct ContentView: View {
             "circle"
         }
     }
-    
+
     func fetchReadings() async {
-//        do {
-//            let url = URL(string: "https://hws.dev/readings.json")!
-//            let (data, _) = try await URLSession.shared.data(from: url)
-//            let readings = try JSONDecoder().decode([Double].self, from: data)
-//            output = "Found \(readings.count) readings"
-//        } catch {
-//            print("Download error")
-//        }
+        //        do {
+        //            let url = URL(string: "https://hws.dev/readings.json")!
+        //            let (data, _) = try await URLSession.shared.data(from: url)
+        //            let readings = try JSONDecoder().decode([Double].self, from: data)
+        //            output = "Found \(readings.count) readings"
+        //        } catch {
+        //            print("Download error")
+        //        }
         let fetchTask = Task {
             let url = URL(string: "https://hws.dev/readings.json")!
             let (data, _) = try await URLSession.shared.data(from: url)
             let readings = try JSONDecoder().decode([Double].self, from: data)
             return "Found \(readings.count) readings"
         }
-        
+
         let result = await fetchTask.result
-        
+
         switch result {
         case .success(let str):
             output = str
